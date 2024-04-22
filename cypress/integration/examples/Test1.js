dffd4cd0015d4a22be68756067c7e1f86debbaa2
happy0412 <siphahle.nomnganga@gmail.com>
2024-04-22 17:00:43 +0200

    Made few updates on the test script

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -6,5 +6,21 @@ describe('My First Test', function(){
         cy.get('.search-keyword').type('ca')
         cy.wait(2000)
         cy.get('.product:visible').should('have.length',4)
+
+        // Parent Child chaining
+        cy.get('.products').find('.product').should('have.length',4)
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+        cy.get('.products').find('.product').each(($el, index, $list)=>{
+
+            const textVeg=$el.find('h4.product-name').text()
+
+            if (textVeg.includes('Cashews'))
+            {
+              cy.wrap($el).find('button').click()
+            }
+        })
+
+        
     })
 })
