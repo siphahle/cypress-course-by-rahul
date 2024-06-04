907a3fcc64c19a1e83ccc2b34c5942b3724d7ab4
happy0412 <siphahle.nomnganga@gmail.com>
2024-06-04 16:03:44 +0200

    added some tests

---
+++ cypress/integration/examples/Test4.js
@@ -0,0 +1,11 @@
+//cypress - Spec
+///<reference types ="Cypress" />
+describe('My Second Test', function(){
+    it('Does not do much!', function(){
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+        
+    })
+    
+})
+
